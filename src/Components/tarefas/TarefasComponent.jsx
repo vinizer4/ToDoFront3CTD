3711f26cc74527166a6ff9c77e3aa5b009032d97
vinizer4 <vinicius.ts.online@gmail.com>
2023-03-15 03:56:32 -0300

    fix

--- src/Components/tarefas/TarefasComponent.jsx
+++ src/Components/tarefas/TarefasComponent.jsx
@@ -1,300 +1,282 @@
-import { useState } from "react";
-import { BsTrash, BsPencil } from "react-icons/bs";
-import { Button, Form, ListGroup, Container, Card } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import {
+	BsTrash,
+	BsPencil
+}                                     from "react-icons/bs";
+import {
+	Button,
+	Form,
+	ListGroup,
+	Container,
+	Card,
+	Badge
+}                                     from "react-bootstrap";
 
 function Tarefas() {
-  const [id, setId] = useState("");
-  const [titulo, setTitulo] = useState("");
-  const [categoria, setCategoria] = useState("");
-  const [data, setData] = useState("")
-  const [descricao, setDescricao] = useState("")
-  const [listaTarefas, setListTarefas] = useState([]);
-  const [validado, setValidado] = useState(false)
-  
-  const [message, setMessage] = useState({
-    titulo: "",
-    data: "",
-    categoria: "",
-    descricao: ""
-                                         })
-  
-  async function valideFields() {
-    
-    setMessage({
-      titulo: "",
-      data: "",
-      categoria: "",
-      descricao: ""
-               })
-    
-    if (titulo === "") {
-      setMessage((prevState) => {
-        return {
-          ...prevState,
-          titulo: "Preencher o campo titulo!",
-          valido: false
-        };
-      });
-    } else if (
-        titulo.length < 3
-    ) {
-      setMessage((prevState) => {
-        return {
-          ...prevState,
-          titulo: "Titulo precisa ter mais de 3 caracteres!",
-        };
-      });
-    }
-  
-    if (categoria === "") {
-      setMessage((prevState) => {
-        return {
-          ...prevState,
-          titulo: "Selecione uma categoria!",
-          valido: false
-        };
-      });
-    }
-  
-    if (descricao === "") {
-      setMessage((prevState) => {
-        return {
-          ...prevState,
-          descricao: "Preencher o campo descricao!",
-          valido: false
-        };
-      });
-    } else if (
-        descricao.length < 3
-    ) {
-      setMessage((prevState) => {
-        return {
-          ...prevState,
-          descricao: "Descricao precisa ter mais de 3 caracteres!",
-          valido: false
-        };
-      });
-    }
-
-// Expressão regular para validar o formato da data
-    const regexData = /^\d{2}\/\d{2}\/\d{4}$/;
-
-// Verifica se a data está no formato correto
-    if (regexData.test(data)) {
-      // Separa o dia, mês e ano da data
-      const partesData = data.split("/");
-      const dia = partesData[0];
-      const mes = partesData[1] - 1; // lembre-se que o mês começa em zero no objeto Date()
-      const ano = partesData[2];
-    
-      // Cria um objeto Date com a data informada
-      const dataInformada = new Date(ano, mes, dia);
-    
-      // Verifica se a data informada é anterior à data de hoje
-      if (dataInformada < new Date()) {
-        setMessage(prevState => {
-          return {
-            ...prevState,
-            data: "A data nao pode ser anterior a data atual"
-          }
-        })
-      }
-    } else {
-      setMessage(prevState => {
-        return {
-          ...prevState,
-          data: "Insira uma data no formato correto dd/mm/yyyy!"
-        }
-      })
-    }
-  
-    await validateMessage()
-    console.log(validado)
-    
-    return validado
-  }
-  
-  function validateMessage() {
-    console.log(message)
-    if (Object.values(message).some((value) => value !== "")) {
-      setValidado(false)
-    } else {
-      setValidado(true)
-    }
-  
-  }
-  
-  async function addItem(event) {
-    event.preventDefault();
-  
-    const isValid = await valideFields()
-  
-  
-    if (isValid === true) {
-      setListTarefas([
-                       ...listaTarefas,
-                       {
-                         id: Date.now(),
-                         titulo: titulo,
-                         categoria: categoria,
-                         data: data,
-                         descricao: descricao
-                       },
-                     ]);
-  
-      setTitulo("");
-      setCategoria("");
-      setData("");
-      setId("")
-      setDescricao("")
-    } else {
-      setListTarefas(prevState => {
-        return {prevState}
-      })
-    }
-  }
-  
-  async function editItem(event) {
-    event.preventDefault();
-  
-    const isValid = await valideFields()
-    
-    console.log(isValid)
-    
-    console.log(listaTarefas)
-    const copyListaTarefas = [...listaTarefas]
-    
-    const index = copyListaTarefas.findIndex(
-        (tarefa) => tarefa.id === id
-    );
-  
-    if (isValid === true) {
-      copyListaTarefas[index].titulo = titulo;
-      copyListaTarefas[index].categoria = categoria;
-      copyListaTarefas[index].data = data;
-      copyListaTarefas[index].descricao = descricao;
-  
-      setListTarefas(copyListaTarefas);
-    } else {
-      setListTarefas(prevState => {return {prevState}})
-    }
-    console.log(listaTarefas)
-  }
-  
-  function apagarItem(id) {
-    if (confirm("Deseja realmente apagar o item?")) {
-      const result = listaTarefas.filter((item) => item.id !== id);
-      setListTarefas(result);
-    }
-  }
-  
-  function preencheEstados(item) {
-    setTitulo(item.titulo);
-    setCategoria(item.categoria);
-    setId(item.id);
-    setData(item.data)
-    setDescricao(item.descricao)
-  }
-  
-  return (
-      <Container>
-        <div className="App">
-          <h1>Cadastrar Tarefas</h1>
-    
-          <Form onSubmit={id ? editItem : addItem}>
-            <Form.Group>
-              <Form.Control
-                  
-                  value={titulo}
-                  onChange={(event) => setTitulo(event.target.value)}
-                  placeholder="Titulo"
-                  className={message.titulo ? "is-invalid" : ""}
-              />
-              {message.titulo && <div className="invalid-feedback">{message.titulo}</div>}
-            </Form.Group>
-            <br/>
-            <Form.Group>
-              <Form.Select
-                  required
-                  value={categoria}
-                  onChange={(event) => setCategoria(event.target.value)}
-                  placeholder={"Categoria"}
-                  className={message.categoria ? "is-invalid" : ""}
-              >
-                <option value="">Selecione uma Categoria</option>
-                <option value="Urgente">Urgente</option>
-                <option value="Alta">Alta</option>
-                <option value="Media">Media</option>
-                <option value="Baixa">Baixa</option>
-              </Form.Select>
-              {message.categoria && <div className="invalid-feedback">{message.categoria}</div>}
-            </Form.Group>
-            <br/>
-            <Form.Group>
-              <Form.Control
-                  value={data}
-                  onChange={(event) => setData(event.target.value)}
-                  placeholder="Data"
-                  className={message.data ? "is-invalid" : ""}
-              />
-              {message.data && <div className="invalid-feedback">{message.data}</div>}
-            </Form.Group>
-            <br/>
-            <Form.Group>
-              <Form.Control
-                  value={descricao}
-                  onChange={(event) => setDescricao(event.target.value)}
-                  placeholder="Descricao"
-                  className={message.descricao ? "is-invalid" : ""}
-              />
-              {message.descricao && <div className="invalid-feedback">{message.descricao}</div>}
-            </Form.Group>
-            <br/>
-            <Button variant="primary" type="submit">
-              {id ? "Salvar" : "Cadastrar"}
-            </Button>
-          </Form>
-          <br/>
-          {listaTarefas.length > 0 ? (
-              <>
-                <h2>Minhas tarefas</h2>
-                {listaTarefas.map((item) => (
-                    <>
-                      <br/>
-                      <Card key={item.id}>
-                        <Card.Header as="h5">{item.titulo}</Card.Header>
-                        <Card.Body>
-                          <Card.Subtitle>{item.categoria}</Card.Subtitle>
-                          <Card.Text>
-                            {item.descricao}
-                          </Card.Text>
-                          <Button
-                              variant="danger"
-                              onClick={() => apagarItem(item.id)}
-                              className="me-2"
-                          >
-                            <BsTrash />
-                          </Button>
-  
-                          <Button
-                              variant="warning"
-                              onClick={() => preencheEstados(item)}
-                          >
-                            <BsPencil />
-                          </Button>
-                        </Card.Body>
-                      </Card>
-                      <br/>
-                    </>
-                ))}
-              </>
-          ) : (
-               <p>Nenhum item cadastrado</p>
-           )}
-        </div>
-      </Container>
-      
-  );
+	const [ id, setId ] = useState("");
+	const [ titulo, setTitulo ] = useState("");
+	const [ categoria, setCategoria ] = useState("");
+	const [ data, setData ] = useState("");
+	const [ descricao, setDescricao ] = useState("");
+	const [ listaTarefas, setListTarefas ] = useState([]);
+	const [ descricaoMessage, setDescricaoMessage ] = useState("")
+	const [ categoriaMessage, setCategoriaMessage ] = useState("")
+	const [ dataMessage, setDataMessage ] = useState("")
+	const [ tituloMessage, setTituloMessage ] = useState("")
+	
+	async function validateTitulo() {
+		if ( titulo === "" ) {
+			setTituloMessage("Preencher o campo titulo!");
+		}
+		else if ( titulo.length < 3 ) {
+			setTituloMessage(
+				"Titulo precisa ter mais de 3 caracteres!");
+		}
+		else {
+			setTituloMessage("");
+		}
+	}
+	
+	async function validateDescricao() {
+		if ( descricao === "" ) {
+			setDescricaoMessage("Preencher o campo descricao!");
+		}
+		else if ( descricao.length < 3 ) {
+			setDescricaoMessage(
+				"Descricao precisa ter mais de 3 caracteres!");
+		}
+		else {
+			setDescricaoMessage("");
+		}
+	}
+	
+	async function validData() {
+		const regexData = /^\d{2}\/\d{2}\/\d{4}$/;
+		
+		setDataMessage("");
+		
+		if ( regexData.test(data) ) {
+			const partesData = data.split("/");
+			const dia = partesData[ 0 ];
+			const mes = partesData[ 1 ] - 1;
+			const ano = partesData[ 2 ];
+			
+			const dataInformada = new Date(ano, mes, dia);
+			
+			if ( dataInformada < new Date() ) {
+				setDataMessage("A data nao pode ser anterior a data"
+				               + " atual")
+			}
+		}
+		else {
+			setDataMessage("Insira uma data no formato "
+			               + " dd/mm/yyyy!")
+		}
+	}
+	
+	function validateFields() {
+		validateDescricao()
+		validateTitulo()
+		validData()
+	}
+	async function addItem(event) {
+		event.preventDefault();
+		await validateFields()
+		console.log("addItem called");
+		if (
+			descricaoMessage === "" &&
+			tituloMessage === "" &&
+			categoriaMessage === "" &&
+			dataMessage === ""
+		) {
+			const randomNumber = Math.floor(Math.random() * 1000000);
+			const copyArray = [...listaTarefas]
+			setListTarefas([
+				               ...copyArray,
+				               {
+					               id: randomNumber,
+					               titulo,
+					               categoria,
+					               data,
+					               descricao,
+				               },
+			               ]);
+			setTitulo("");
+			setCategoria("");
+			setData("");
+			setId("");
+			setDescricao("");
+		}
+	}
+	
+	function handleSubmit(event) {
+		event.preventDefault();
+		console.log(event)
+		console.log("form submitted");
+		addItem(event);
+	}
+	
+	useEffect(() => {
+		console.log(listaTarefas)
+	}, [listaTarefas])
+	
+	
+	async function editItem(event) {
+		event.preventDefault();
+		await validateFields();
+		if ( message.isValid ) {
+			const index = listaTarefas.findIndex((tarefa) => tarefa.id
+			                                                 === id);
+			const copyListaTarefas = [ ...listaTarefas ];
+			copyListaTarefas[ index ] = {
+				...copyListaTarefas[ index ],
+				titulo,
+				categoria,
+				data,
+				descricao,
+			};
+			setListTarefas(copyListaTarefas);
+		}
+	}
+	
+	function apagarItem(id) {
+		if ( confirm("Deseja realmente apagar o item?") ) {
+			const result = listaTarefas.filter((item) => item.id
+			                                             !== id);
+			setListTarefas(result);
+		}
+	}
+	
+	function preencheEstados(item) {
+		setTitulo(item.titulo);
+		setCategoria(item.categoria);
+		setId(item.id);
+		setData(item.data);
+		setDescricao(item.descricao);
+	}
+	
+	return (
+		<Container>
+			<div className="App">
+				<h1>Cadastrar Tarefas</h1>
+				
+				<Form onSubmit={ handleSubmit }>
+					<Form.Group>
+						<Form.Control
+							onClick={ validateTitulo }
+							value={ titulo }
+							onChange={ (event) => {
+								setTitulo(event.target.value)
+								validateTitulo()
+							} }
+							placeholder="Titulo"
+							className={ tituloMessage
+							            ? "is-invalid"
+							            : "" }
+						/>
+						{ tituloMessage && <div
+							className="invalid-feedback">{ tituloMessage }</div> }
+					</Form.Group>
+					<br/>
+					<Form.Group>
+						<Form.Select
+							required
+							value={ categoria }
+							onChange={ (event) => setCategoria(event.target.value) }
+							placeholder={ "Categoria" }
+							className={ categoriaMessage
+							            ? "is-invalid"
+							            : "" }
+						>
+							<option value="">Selecione uma Categoria
+							</option>
+							<option value="Urgente">Urgente</option>
+							<option value="Alta">Alta</option>
+							<option value="Media">Media</option>
+							<option value="Baixa">Baixa</option>
+						</Form.Select>
+						{ categoriaMessage && <div
+							className="invalid-feedback">{ categoriaMessage }</div> }
+					</Form.Group>
+					<br/>
+					<Form.Group>
+						<Form.Control
+							value={ data }
+							onClick={ validData }
+							onChange={ (event) => {
+								setData(event.target.value)
+								validData()
+							} }
+							placeholder="Data"
+							className={ dataMessage
+							            ? "is-invalid"
+							            : "" }
+						/>
+						{ dataMessage && <div
+							className="invalid-feedback">{ dataMessage }</div> }
+					</Form.Group>
+					<br/>
+					<Form.Group>
+						<Form.Control
+							onClick={ validateDescricao }
+							value={ descricao }
+							onChange={ (event) => {
+								setDescricao(event.target.value)
+								validateDescricao()
+							} }
+							placeholder="Descricao"
+							className={ descricaoMessage
+							            ? "is-invalid"
+							            : "" }
+						/>
+						{ descricaoMessage && <div
+							className="invalid-feedback">{ descricaoMessage }</div> }
+					</Form.Group>
+					<br/>
+					<Button variant="primary" type="submit">
+						{ id ? "Salvar" : "Cadastrar" }
+					</Button>
+				</Form>
+				<br/>
+				{listaTarefas.length > 0 ? (
+					<>
+						<h2>Minhas tarefas</h2>
+						{listaTarefas.map((item) => (
+							<React.Fragment key={item.id}>
+								<ListGroup>
+									<ListGroup.Item>
+										<h5>{item.titulo}</h5>
+										<Badge bg="secondary">{item.categoria}</Badge>{' '}
+										<span className="text-muted">{item.data}</span>
+									</ListGroup.Item>
+									<ListGroup.Item>{item.descricao}</ListGroup.Item>
+									<ListGroup.Item>
+										<Button
+											variant="danger"
+											onClick={() => apagarItem(item.id)}
+											className="me-2"
+										>
+											<BsTrash />
+										</Button>
+										<Button
+											variant="warning"
+											onClick={() => preencheEstados(item)}
+										>
+											<BsPencil />
+										</Button>
+									</ListGroup.Item>
+								</ListGroup>
+								<br />
+							</React.Fragment>
+						))}
+					</>
+				) : (
+					 <p>Nenhum item cadastrado</p>
+				 )}
+			</div>
+		</Container>
+	
+	);
 }
 
 export default Tarefas;
